8d6177f9e82bece23480a08fe07b2076b591acc5
Alessandro <cesaro.alessandro@iisviolamarchesini.edu.it>
2022-01-19 17:40:27 +0100

    Fix1

--- esercizio1.c
+++ esercizio1.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-
-double numero1;
-double numero2;
-double rapporto;
-printf("Inserisci un numero\n")
-scanf(%d, & numero1)
-printf("Inserisci un secondo numero\n")
-scanf(%d, & numero2)
-do
+int main (int argc, char *argv[])
 {
-    if (numero1 > numero2)
+
+    double numero1;
+    double numero2;
+    double rapporto;
+    printf("Inserisci un numero \n")
+    scanf(%d, & numero1)
+    printf("Inserisci un secondo numero \n")
+    scanf(%d, & numero2)
+    do
     {
-        numero1 = numero1/numero2
-    }
-    else if (numero2 > numero1)
-    {
-        numero2 = numero2/numero1
-    }
+        if (numero1 > numero2)
+        {
+            numero1 = numero1/numero2
+        }
+        else if (numero2 > numero1)
+        {
+            numero2 = numero2/numero1
+        }
     
-} while (numero1 != 0 || numero1 != 0);
-printf(numero1)
-printf(numero2)
+    } while (numero1 != 0 || numero1 != 0);
+    printf(numero1)
+    printf(numero2)
+}
