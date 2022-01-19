d0eb3c590499dc4705bd6af764866bef9e2482c9
Alessandro <cesaro.alessandro@iisviolamarchesini.edu.it>
2022-01-19 17:41:54 +0100

    Fix2

--- esercizio1.c
+++ esercizio1.c
@@ -5,22 +5,22 @@ int main (int argc, char *argv[])
     double numero1;
     double numero2;
     double rapporto;
-    printf("Inserisci un numero \n")
-    scanf(%d, & numero1)
-    printf("Inserisci un secondo numero \n")
-    scanf(%d, & numero2)
+    printf("Inserisci un numero \n");
+    scanf(%d, & numero1);
+    printf("Inserisci un secondo numero \n");
+    scanf(%d, & numero2);
     do
     {
         if (numero1 > numero2)
         {
-            numero1 = numero1/numero2
+            numero1 = numero1/numero2;
         }
         else if (numero2 > numero1)
         {
-            numero2 = numero2/numero1
+            numero2 = numero2/numero1;
         }
     
     } while (numero1 != 0 || numero1 != 0);
-    printf(numero1)
-    printf(numero2)
+    printf(numero1);
+    printf(numero2);
 }
