c44d61ba6d19f0daac973fb4cef31d72b6725c8a
Alessandro <cesaro.alessandro@iisviolamarchesini.edu.it>
2022-01-20 14:44:20 +0100

    Fix19

--- esercizio1.c
+++ esercizio1.c
@@ -10,21 +10,14 @@ int main(int argc, char *argv[])
 
         printf("Inserisci un numero \n");
         scanf("%lf", &numero1);
-        printf("rapporto \n");
         printf("Inserisci un secondo numero \n");
         scanf("%lf", &numero2);
-        printf("rapporto \n");
-        printf("%lf \n", numero2);
         if (numero1 > numero2){
-            printf("rapporto \n");
             rapporto = numero1 / numero2;
-            printf("rapporto \n");
             printf("%lf \n", rapporto);
         }
         else if (numero2 > numero1){
-            printf("rapporto \n");
             rapporto = numero2 / numero1;
-            printf("rapporto \n");
             printf("%lf \n", rapporto);
         }
     } while (numero1 != 0 || numero2 != 0);
